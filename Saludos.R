0e9952883fef87bfb9279be8e4335927ca9c03cc
Alejandra Bahena <ale--bo@hotmail.com>
2016-03-04 15:28:11 -0600

    Funcionamiento
    Prueba

--- Saludos.R
+++ Saludos.R
@@ -13,3 +13,23 @@ if(x>.3 && x<.4) {
     y <- "Qué pedo wey?"
 }
 y
+
+# prueba de funcionamiento
+n <- 100
+mensajes <- vector("character",n)
+for(i in 1:n) {
+    y <- runif(1)
+    if (y <= 0.30){
+        mensajes[i] <- "Mensaje 30%"
+    } else if (y <= .50){
+        mensajes [i] <- "Mensaje 20% a"
+    } else if (y <= .70){
+        mensajes[i] <- "Mensaje 20% b"
+    } else if (y <= .90){
+        mensajes[i] <- "Mensaje 20% c"
+    } else{
+        mensajes[i] <- "Mensaje 10%"
+    }
+}
+plot(table(mensajes))
+
